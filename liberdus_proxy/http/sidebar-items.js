9578591731f8a733037873d622c1831c4a8fba33
kgmyatthu <kgmyatthu@users.noreply.github.com>
2024-12-30 10:37:21 +0000

    deploy: 98da33b57048d025cfe29b3030819ea1ed48d7d9

--- liberdus_proxy/http/sidebar-items.js
+++ liberdus_proxy/http/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["handle_client","parse_content_length","read_or_collect","respond_with_internal_error","respond_with_timeout"]};
+window.SIDEBAR_ITEMS = {"fn":["handle_stream","parse_content_length","read_or_collect","respond_with_internal_error","respond_with_timeout"]};
