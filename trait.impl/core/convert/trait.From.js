516feba99471c8eb217389a1bb8e74851e8167f8
kgmyatthu <kgmyatthu@users.noreply.github.com>
2025-05-21 05:29:52 +0000

    deploy: c742d9f5fc2fdcfca5a5c7414f08ddadd8d26554

---
+++ trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["liberdus_proxy",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.82.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"liberdus_proxy/subscription/enum.SubscriptionActions.html\" title=\"enum liberdus_proxy::subscription::SubscriptionActions\">SubscriptionActions</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[450]}
