c6859d3d81273c303e195184300e7bff5f0fe1cf
kgmyatthu <kgmyatthu@users.noreply.github.com>
2025-12-11 02:27:32 +0000

    deploy: f25d4ac45fb2f754d228756eeda730629b3c890a

---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["liberdus_proxy",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"liberdus_proxy/config/struct.Config.html\" title=\"struct liberdus_proxy::config::Config\">Config</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[306]}
