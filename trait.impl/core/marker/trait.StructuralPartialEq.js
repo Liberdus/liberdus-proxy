a443210057ac2c06c8cd9952ee85579ec42753eb
kgmyatthu <kgmyatthu@users.noreply.github.com>
2025-12-11 02:07:23 +0000

    deploy: d3a03cda75bc7742040495fc02774adb99aea7b1

--- trait.impl/core/marker/trait.StructuralPartialEq.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["liberdus_proxy",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"liberdus_proxy/rpc/struct.RpcError.html\" title=\"struct liberdus_proxy::rpc::RpcError\">RpcError</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[340]}
